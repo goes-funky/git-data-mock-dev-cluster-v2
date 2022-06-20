46b95ab39702f94d2baba95e0331a730deb989e9
Hung Dang <hd@y42.com>
2022-06-20 19:37:13 +0200

    models: update model "hai_testing"

--- src/Models/SQL/hai_testing/table_1/sql-model.sql
+++ src/Models/SQL/hai_testing/table_1/sql-model.sql
@@ -1,1 +1,11 @@
-SELECT * FROM {{ ref('src/Integrations/My_SQL_integration/doctrine_migrations') }} LIMIT 100
+with hag as (
+    SELECT CAST(page_id as INT) page_id FROM {{ ref('src/Integrations/facebook_pages/posts') }}
+),
+
+jake AS (
+    SELECT Id FROM {{ ref('src/Integrations/Google_sheets_all_field_types/Copy of Numeric2-Sheet1') }}
+)
+
+SELECT * page_id FROM hag
+full outer JOIN jake
+on hag.page_id = jake.id
--- src/Models/SQL/hai_testing/table_2/sql-model.sql
+++ src/Models/SQL/hai_testing/table_2/sql-model.sql
@@ -1,1 +1,16 @@
-SELECT * FROM {{ ref('src/Integrations/My_SQL_integration/doctrine_migrations') }} LIMIT 200
+Select 
+  phone,
+  id,
+  country,
+  address1,
+  name,
+  address2,
+  province,
+  city,
+  company,
+  country_code,
+  province_code,
+  zip,
+  created_at,
+  updated_at
+from {{ ref('src/Integrations/shopify/locations') }}
