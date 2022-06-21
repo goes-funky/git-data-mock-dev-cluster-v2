df7e761918fb27bd202d97aec72b9aa4b7c16b60
Lyubov Yunash <lyubov.yunash@y42.com>
2022-06-21 17:59:06 +0300

    models: update model "sql_model"

--- src/Models/SQL/sql_model/table/sql-model.sql
+++ src/Models/SQL/sql_model/table/sql-model.sql
@@ -11,3 +11,18 @@ Select
   lists,
   permission_level,
   space_id
+Select 
+  Id,
+  Pozitive_numbers,
+  Decimal_numbers,
+  Negative,
+  Negative_double,
+  Text,
+  Id_repeatable,
+  Id_text_repeatable_,
+  Id_of_mix,
+  Boolean,
+  Date,
+  Date_time,
+  Time
+from {{ ref('src/Integrations/Google_sheets_all_field_types/Copy of Numeric2-Sheet1') }}
--- src/Models/SQL/sql_model/table456/sql-model.sql
+++ src/Models/SQL/sql_model/table456/sql-model.sql
@@ -1,1 +1,17 @@
-
+Select 
+  page_id,
+  business_account_id,
+  date,
+  follower_count,
+  get_directions_clicks,
+  impressions,
+  phone_call_clicks,
+  profile_views,
+  reach,
+  text_message_clicks,
+  website_clicks,
+  impressions_week,
+  reach_week,
+  impressions_days_28,
+  reach_days_28
+from {{ ref('src/Integrations/instagram/user_insights') }}
