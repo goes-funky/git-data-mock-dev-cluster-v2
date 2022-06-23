44dcad5d6918484382b1e127b6ec1b5fae184327
Hung Dang <hd@y42.com>
2022-06-23 17:53:21 +0200

    test

--- src/Models/SQL/hai_testing/table_1/sql-model.sql
+++ src/Models/SQL/hai_testing/table_1/sql-model.sql
@@ -1,11 +1,22 @@
-Select 
-  name,
-  period,
-  value,
-  context,
-  end_time,
-  title,
-  description,
-  id,
-  page_id
-from {{ ref('src/Integrations/facebook_pages/page_insight_website_clicks') }}
+with hag as (
+  SELECT 
+  name, 
+  period, 
+  value, 
+  context, 
+  end_time, 
+  title, 
+  description, 
+  id
+   FROM {{ ref('src/Integrations/facebook_pages/page_insight_website_clicks') }}),
+
+john as (SELECT 
+page_id, 
+allowed_advertising_objects, 
+created_time, 
+full_picture, 
+icon from {{ ref('src/Integrations/facebook_pages/posts') }})
+
+SELECT * FROM hag
+JOIN john
+on hag.value = john.page_id
---
+++ src/Models/SQL/new_testing_hai/table_2/sql-model.sql
@@ -0,0 +1,11 @@
+Select 
+  name,
+  period,
+  value,
+  context,
+  end_time,
+  title,
+  description,
+  id,
+  page_id
+from {{ ref('src/Integrations/facebook_pages/page_insight_website_clicks') }}
